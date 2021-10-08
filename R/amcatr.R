772cf11a3c44e27ee7547fe4ad0d71a70aba5acc
Wouter van Atteveldt <wouter@vanatteveldt.com>
2021-10-08 11:45:43 +0200

    add get_codingjob function

--- R/amcatr.R
+++ R/amcatr.R
@@ -51,3 +51,15 @@ post_codingjob <- function(codingjob, credentials=NULL) {
   httr::stop_for_status(r)
   httr::content(r)$id
 }
+
+#' Retrieve a codingjob (including results)
+#' @param id the job ID
+#' @param credentials The credentials to use. If not given, use last login information
+#' @export
+get_codingjob <- function(id, credentials=NULL) {
+  credentials <- get_credentials(credentials)
+  r <- httr::GET(paste0(credentials$host, "/codingjob/", id),
+                 httr::add_headers(Authorization = paste("Bearer", credentials$token)))
+  httr::stop_for_status(r)
+  httr::content(r)
+}
