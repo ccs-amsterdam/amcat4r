af2b36efe5a8398621dcea01b6ffa376bbcbe315
JBGruber <gruber@europa-uni.de>
2023-01-23 18:07:23 +0100

    fix gh action

--- tests/testthat/test-docker.R
+++ tests/testthat/test-docker.R
@@ -3,4 +3,5 @@ test_that("docker", {
   # only test ngincat to not interfere with other tests
   expect_no_error(stop_amcat_docker(filters = "ngincat"))
   expect_no_error(run_amcat_docker())
+  Sys.sleep(5) # wait a few seconds until back running
 })
